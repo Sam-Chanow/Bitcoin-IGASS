468e451fb3ffceacaa7ae73d3cfac5cf7c7ebe2b
SamChanow <samchanow@gmail.com>
2021-11-10 14:35:48 -0500

    Updated build_vector_data to not overload RAM

--- model.py
+++ model.py
@@ -19,27 +19,24 @@ def build_vector_data(raw_data):  # raw_data is a dataset object, and this will 
     model = BertModel.from_pretrained('bert-base-uncased', config=config)
 
     # vector data to store to file
-    vd = {}
-    key = 0
+    f_n = 0
 
     # We are not gonna split by sentence right now, insteaad we will split by post, we may change this later
     for post in iter(raw_data):
         label = post[0]
         posts = post[1]
 
-        vd[key] = [label, []]
+        v = [label, []]
 
         for s in tqdm(posts):
             post_v = tokenizer(s, truncation=True, return_tensors="pt", padding=True)
             outputs = model(**post_v)
             data = outputs.last_hidden_state[0][0]
-            vd[key][1].append(data)
-            if key > 1: break
-        if key > 1: break
+            v[1].append(data)
 
-        key += 1
-
-    torch.save(vd, 'data/compiled-datasets/BVBPRI/BVBPRI.pt')
+        torch.save(v, 'data/compiled-datasets/BVBPRI/BVBPRI' + str(f_n) + '.pt')
+        f_n += 1
+        #if f_n > 1: break;
 
 
 # This function will clean a string of data
@@ -63,8 +60,8 @@ class Model:
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1) and (sys.argv[1] == '-vector'):
-        D = dataset.Dataset(['data/compiled-datasets/BPRI-POSTSPLIT/BPRI00.bpri', 'data/compiled-datasets/BPRI-POSTSPLIT/BPRI01.bpri'], dataset.parseBPRI)
+        D = dataset.Dataset(['data/compiled-datasets/BPRI-POSTSPLIT/BPRI00.bpri', 'data/compiled-datasets/BPRI-POSTSPLIT/BPRI01.bpri', 'data/compiled-datasets/BPRI-POSTSPLIT/BPRI02.bpri'], dataset.parseBPRI)
         build_vector_data(D)
     if (len(sys.argv) > 1) and (sys.argv[1] == '-load'):
-        l = torch.load('data/compiled-datasets/BVBPRI/BVBPRI.pt')
-        print(l['1'])
+        l = torch.load('data/compiled-datasets/BVBPRI/BVBPRI0.pt')
+        print(l[1][0])
