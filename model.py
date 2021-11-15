1315900140cf660a100d6a895ba6a203a7d2ee8a
SamChanow <samchanow@gmail.com>
2021-11-15 16:49:22 -0500

    Updated model.py to vector all BVBPRI data. README is not updated. Still looking for solution on how to store BVBPRI dataset becasue it exceeds 80 GB in size.

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ import sys
 
 # This function will use BERT to vectorize a datset and return a new Dataset
 # This function expects a BPRI parsed dataset
-def build_vector_data(raw_data):  # raw_data is a dataset object, and this will make a vectorized dataset for the model
+def build_vector_data(raw_data, start_tok=0):  # raw_data is a dataset object, and this will make a vectorized dataset for the model
     # Vector data writes the vectorized data to a file and returns a dataset object pointing to that file
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -19,27 +19,26 @@ def build_vector_data(raw_data):  # raw_data is a dataset object, and this will 
     model = BertModel.from_pretrained('bert-base-uncased', config=config)
 
     # vector data to store to file
-    vd = {}
-    key = 0
+    f_n = -1
 
     # We are not gonna split by sentence right now, insteaad we will split by post, we may change this later
     for post in iter(raw_data):
+        f_n += 1
+        if f_n < start_tok: continue
+        # The line above lets you pickup at a different file
         label = post[0]
         posts = post[1]
 
-        vd[key] = [label, []]
+        v = [label, []]
 
         for s in tqdm(posts):
             post_v = tokenizer(s, truncation=True, return_tensors="pt", padding=True)
             outputs = model(**post_v)
             data = outputs.last_hidden_state[0][0]
-            vd[key][1].append(data)
-            if key > 1: break
-        if key > 1: break
+            v[1].append(data)
 
-        key += 1
-
-    torch.save(vd, 'data/compiled-datasets/BVBPRI/BVBPRI.pt')
+        torch.save(v, 'data/compiled-datasets/BVBPRI/BVBPRI' + str(f_n) + '.pt')
+        #if f_n > 1: break;
 
 
 # This function will clean a string of data
@@ -63,8 +62,8 @@ class Model:
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1) and (sys.argv[1] == '-vector'):
-        D = dataset.Dataset(['data/compiled-datasets/BPRI-POSTSPLIT/BPRI00.bpri', 'data/compiled-datasets/BPRI-POSTSPLIT/BPRI01.bpri'], dataset.parseBPRI)
-        build_vector_data(D)
+        D = dataset.Dataset(['data/compiled-datasets/BPRI-POSTSPLIT/BPRI00.bpri', 'data/compiled-datasets/BPRI-POSTSPLIT/BPRI01.bpri', 'data/compiled-datasets/BPRI-POSTSPLIT/BPRI02.bpri'], dataset.parseBPRI)
+        build_vector_data(D, int(sys.argv[2]))
     if (len(sys.argv) > 1) and (sys.argv[1] == '-load'):
-        l = torch.load('data/compiled-datasets/BVBPRI/BVBPRI.pt')
-        print(l['1'])
+        l = torch.load('data/compiled-datasets/BVBPRI/BVBPRI0.pt')
+        print(l[1][0])
---
+++ test.py
@@ -0,0 +1,9 @@
+nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+num = -1
+start = 5
+
+for n in nums:
+    num += 1
+    if num < start: continue
+
+    print(n)
